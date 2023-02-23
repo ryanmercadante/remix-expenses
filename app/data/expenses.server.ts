683ef71cfd33fed2f8f97b5bccffc538ee393455
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2023-02-23 08:36:10 -0500

    add action function to create new expense

--- app/data/expenses.server.ts
+++ app/data/expenses.server.ts
@@ -5,6 +5,11 @@ export async function createExpense({
   title,
   amount,
   date,
-}: Pick<Expense, "title" | "amount" | "date" | "createdAt" | "id">) {
-  prisma.expense.create({ data: { title, amount, date } });
+}: Pick<Expense, "title" | "amount" | "date">) {
+  try {
+    return await prisma.expense.create({ data: { title, amount, date } });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
--- app/routes/__app/expenses/add.tsx
+++ app/routes/__app/expenses/add.tsx
@@ -1,7 +1,17 @@
 import type { ActionFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import { ExpenseForm } from "~/components/expenses/ExpenseForm";
 import { Modal } from "~/components/util/Modal";
+import { createExpense } from "~/data/expenses.server";
+
+type ActionData = {
+  errors?: {
+    title?: string;
+    amount?: string;
+    date?: string;
+  };
+};
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -17,4 +27,32 @@ export default function AddExpensesPage() {
   );
 }
 
-export const action: ActionFunction = () => {};
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const amount = formData.get("amount");
+  const date = formData.get("date");
+
+  if (typeof title !== "string" || title.length < 3) {
+    return json<ActionData>(
+      { errors: { title: "Title must be at least 3 characters" } },
+      { status: 400 },
+    );
+  }
+  if (isNaN(Number(amount))) {
+    return json<ActionData>(
+      { errors: { amount: "Amount must be a number" } },
+      { status: 400 },
+    );
+  }
+  if (typeof date !== "string" || isNaN(new Date(date).getTime())) {
+    return json<ActionData>(
+      { errors: { date: "Invalid date" } },
+      { status: 400 },
+    );
+  }
+
+  await createExpense({ title, amount: Number(amount), date: new Date(date) });
+  return redirect("/expenses");
+};
