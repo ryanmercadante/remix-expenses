14eda224c80ffaa841a79ed3c3fc844fa47ed2bb
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2023-02-26 15:41:13 -0500

    start auth action

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,7 +1,8 @@
-import { Link, useSearchParams } from "@remix-run/react";
+import { Form, Link, useNavigation, useSearchParams } from "@remix-run/react";
 import { FaLock, FaUserPlus } from "react-icons/fa";
 
 export function AuthForm() {
+  const navigation = useNavigation();
   const [searchParams] = useSearchParams();
   const authMode = searchParams.get("mode") || "login";
 
@@ -9,8 +10,10 @@ export function AuthForm() {
   const toggleBtnCaption =
     authMode === "login" ? "Create a new user" : "Log in with existing user";
 
+  const isSubmitting = navigation.state !== "idle";
+
   return (
-    <form method="post" className="form" id="auth-form">
+    <Form method="post" className="form" id="auth-form">
       <div className="icon-img">
         {authMode === "login" ? <FaLock /> : <FaUserPlus />}
       </div>
@@ -23,11 +26,13 @@ export function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>{submitBtnCaption}</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Authenticating..." : submitBtnCaption}
+        </button>
         <Link to={`?mode=${authMode === "login" ? "signup" : "login"}`}>
           {toggleBtnCaption}
         </Link>
       </div>
-    </form>
+    </Form>
   );
 }
--- app/routes/__marketing/auth.tsx
+++ app/routes/__marketing/auth.tsx
@@ -1,6 +1,24 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { ActionArgs, LinksFunction } from "@remix-run/node";
 import { AuthForm } from "~/components/auth/AuthForm";
 import styles from "~/styles/auth.css";
+
+export async function action({ request }: ActionArgs) {
+  const { searchParams } = new URL(request.url);
+  const authMode = searchParams.get("mode") || "login";
+
+  const formData = await request.formData();
+
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  // validate user input
+
+  if (authMode === "login") {
+    // login logic
+  } else {
+    // signup logic (create user)
+  }
+}
 
 export default function AuthPage() {
   return <AuthForm />;
