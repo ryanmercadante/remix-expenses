e50262e1205be4421c492d2763b530635196b6ed
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2023-02-26 16:38:29 -0500

    fix expense analysis and raw to call db

--- app/routes/__app/expenses.analysis.tsx
+++ app/routes/__app/expenses.analysis.tsx
@@ -1,11 +1,55 @@
+import { type LoaderFunction, json } from "@remix-run/node";
+import {
+  CatchBoundaryComponent,
+  useCatch,
+  useLoaderData,
+} from "@remix-run/react";
 import { Chart } from "~/components/expenses/Chart";
 import { ExpenseStatistics } from "~/components/expenses/ExpenseStatistics";
+import { Error } from "~/components/util/Error";
+import { getExpenses } from "~/data/expenses.server";
+
+type LoaderError = {
+  message: string;
+};
+
+type LoaderData = {
+  expenses: Awaited<ReturnType<typeof getExpenses>>;
+};
+
+export const loader: LoaderFunction = async () => {
+  const expenses = await getExpenses();
+  if (!expenses || expenses.length === 0) {
+    throw json<LoaderError>(
+      { message: "Could not load expenses for the requested analysis." },
+      { status: 404, statusText: "Expenses not found" },
+    );
+  }
+  return json<LoaderData>({ expenses });
+};
 
 export default function ExpenseAnalysisPage() {
+  const { expenses } = useLoaderData() as unknown as LoaderData;
+
   return (
     <main>
-      <Chart expenses={DUMMY_EXPENSES} />
-      <ExpenseStatistics expenses={DUMMY_EXPENSES} />
+      <Chart expenses={expenses} />
+      <ExpenseStatistics expenses={expenses} />
     </main>
   );
 }
+
+export const CatchBoundary: CatchBoundaryComponent = () => {
+  const caughtResponse = useCatch();
+
+  return (
+    <main>
+      <Error title={caughtResponse.statusText}>
+        <p>
+          {caughtResponse.data?.message ||
+            "Something went wrong - could not load expenses."}
+        </p>
+      </Error>
+    </main>
+  );
+};
